628efbbcdb0280e89fabab1213aefd4ebbb2107f
krupali-27 <133683711+krupali-27@users.noreply.github.com>
2023-05-29 22:06:34 +0530

    Add files via upload

---
+++ Circle.java
@@ -0,0 +1,36 @@
+import java.util.*;
+public class Circle 
+{
+    double radius;
+    double area; 
+    double circumference;
+    final double PI=3.14;
+      
+    
+    public void setData()
+    {
+        Scanner sc=new Scanner(System.in);
+        System.out.println("enter radius");
+        radius=sc.nextFloat();
+        
+    }
+    public void calArea()
+    {
+        area=PI*radius*radius;
+        System.out.println("Area of the circle is:"+area);
+    }
+    public void calCircumference()
+    {
+        this.circumference=2*PI*this.radius;
+        System.out.println("Circumference of the circle is:"+circumference);
+    }
+   public static void main(String args[])
+   {
+       Circle c1=new Circle();
+       c1.setData();
+       c1.calArea();
+       Circle c2=new Circle();
+       c2.setData();
+       c2.calArea();
+   }
+}
