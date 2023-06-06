b0932eb442b1b40fbbcb21f433b8d5b8fd55dd3e
krupali-27 <133683711+krupali-27@users.noreply.github.com>
2023-06-07 00:48:40 +0530

    Add files via upload

---
+++ Employee.java
@@ -0,0 +1,31 @@
+import java.util.*;
+ class EmpCount 
+{
+    static int count_of_object=0;
+    
+    public EmpCount()
+    {
+        count_of_object++;
+    }
+    public  static int getCount()
+    {
+        return count_of_object;
+    }
+    
+}
+
+public class Employee
+{
+    public static void main(String args[])
+    {
+        Scanner sc=new Scanner(System.in);
+        System.out.println("enter size of the array");
+        int arr_size=sc.nextInt();
+        EmpCount e[]=new EmpCount[arr_size];
+        for(int i=0;i<e.length;i++)
+        {
+            e[i]=new EmpCount();
+        }
+        System.out.println("count of number of employee is="+EmpCount.getCount());
+    }
+}
